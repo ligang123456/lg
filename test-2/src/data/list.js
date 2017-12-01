85141369ce02493ae81e565b5a3499db821c1426
A12 <a12@A12s-MacBook-Air.local>
2017-12-01 20:41:55 +0800

    music

--- test-2/src/data/list.js
+++ test-2/src/data/list.js
@@ -36,40 +36,46 @@ module.exports = [
     // pic: 'http://img1.imgtn.bdimg.com/it/u=2553322381,2925312760&fm=27&gp=0.jpg',
     // src: 'http://ws.stream.qqmusic.qq.com/M500001VfvsJ21xFqb.mp3?guid=ffffffff82def4af4b12b3cd9337d5e7&uin=346897220&vkey=6292F51E1E384E06DCBDC9AB7C49FD713D632D313AC4858BACB8DDD29067D3C601481D36E62053BF8DFEAF74C0A5CCFADD6471160CAF3E6A&fromtag=46',
     // src: 'http://music.163.com/song/media/outer/url?id=26662115.mp3',
-    src: 'http://music.163.com/song/media/outer/url?id=26662115.mp3',
+    src: 'http://music.163.com/song/media/outer/url?id=30590695.mp3',
     // src: '4',
-    name: 'The Mass',
-    autor: 'Era',
-    pic: 'https://ww4.sinaimg.cn/mw600/7ed838e1jw1dsemiqdn06j.jpg'
+    name: 'Strength Of A Thousand Men',
+    autor: 'Various Artists',
+    pic: 'http://i01.pic.sogou.com/55e4e7db0e90e01d'
   },
   {
     // pic: 'http://img1.imgtn.bdimg.com/it/u=2553322381,2925312760&fm=27&gp=0.jpg',
     // src: 'http://ws.stream.qqmusic.qq.com/M500001VfvsJ21xFqb.mp3?guid=ffffffff82def4af4b12b3cd9337d5e7&uin=346897220&vkey=6292F51E1E384E06DCBDC9AB7C49FD713D632D313AC4858BACB8DDD29067D3C601481D36E62053BF8DFEAF74C0A5CCFADD6471160CAF3E6A&fromtag=46',
     // src: 'http://music.163.com/song/media/outer/url?id=26662115.mp3',
-    src: 'http://music.163.com/song/media/outer/url?id=26662115.mp3',
+    src: 'http://music.163.com/song/media/outer/url?id=507309525.mp3',
     // src: '4',
-    name: 'The Mass',
-    autor: 'Era',
-    pic: 'https://ww4.sinaimg.cn/mw600/7ed838e1jw1dsemiqdn06j.jpg'
+    name: '80000 （Prod.By DROYC',
+    autor: 'PRC 巴音汗',
+    pic: 'http://pic.xiami.net/images/album/img69/518548869/3779351505377935.jpg@1e_1c_100Q_185w_185h'
   },
   {
     // pic: 'http://img1.imgtn.bdimg.com/it/u=2553322381,2925312760&fm=27&gp=0.jpg',
     // src: 'http://ws.stream.qqmusic.qq.com/M500001VfvsJ21xFqb.mp3?guid=ffffffff82def4af4b12b3cd9337d5e7&uin=346897220&vkey=6292F51E1E384E06DCBDC9AB7C49FD713D632D313AC4858BACB8DDD29067D3C601481D36E62053BF8DFEAF74C0A5CCFADD6471160CAF3E6A&fromtag=46',
     // src: 'http://music.163.com/song/media/outer/url?id=26662115.mp3',
-    src: 'http://music.163.com/song/media/outer/url?id=26662115.mp3',
+    src: 'http://music.163.com/song/media/outer/url?id=479574796.mp3',
     // src: '4',
-    name: 'The Mass',
-    autor: 'Era',
-    pic: 'https://ww4.sinaimg.cn/mw600/7ed838e1jw1dsemiqdn06j.jpg'
+    name: '文爱',
+    autor: 'CG',
+    pic: 'http://pic.xiami.net/images/album/img35/2103335235/1507529617955.jpg@1e_1c_100Q_185w_185h'
   },
   {
     // pic: 'http://img1.imgtn.bdimg.com/it/u=2553322381,2925312760&fm=27&gp=0.jpg',
     // src: 'http://ws.stream.qqmusic.qq.com/M500001VfvsJ21xFqb.mp3?guid=ffffffff82def4af4b12b3cd9337d5e7&uin=346897220&vkey=6292F51E1E384E06DCBDC9AB7C49FD713D632D313AC4858BACB8DDD29067D3C601481D36E62053BF8DFEAF74C0A5CCFADD6471160CAF3E6A&fromtag=46',
     // src: 'http://music.163.com/song/media/outer/url?id=26662115.mp3',
-    src: 'http://music.163.com/song/media/outer/url?id=26662115.mp3',
+    src: 'http://music.163.com/song/media/outer/url?id=452986458.mp3',
     // src: '4',
-    name: 'The Mass',
-    autor: 'Era',
-    pic: 'https://ww4.sinaimg.cn/mw600/7ed838e1jw1dsemiqdn06j.jpg'
+    name: '红昭愿',
+    autor: '音阙诗听',
+    pic: 'http://img3.kuwo.cn/star/albumcover/240/14/73/3654364992.jpg'
+  },
+  {
+    src: 'http://music.163.com/song/media/outer/url?id=482395261.mp3',
+    name: '我们不一样',
+    autor: '大壮',
+    pic: 'http://img.zcool.cn/community/01d7255881d5aea801219c776899ba.jpg'
   }
 ]
